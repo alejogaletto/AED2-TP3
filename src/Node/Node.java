41fd2a4e831c88787351ac01a9a352c247e3670b
Marco Mignacco <marcomignacco03@gmail.com>
2022-09-12 20:06:26 -0300

    changes

--- src/Node/Node.java
+++ src/Node/Node.java
@@ -1,5 +1,44 @@
 package Node;
 
 public class Node {
+    private int data;
+    private Node leftChild;
+    private Node rigthChild;
+
+    public Node(int data){
+        this.data = data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+    public int getData() {
+        return data;
+    }
+
+    public void setLeftChild(Node leftChild) {
+        this.leftChild = leftChild;
+    }
+    public Node getLeftChild() {
+        return leftChild;
+    }
+
+    public void setRigthChild(Node rigthChild) {
+        this.rigthChild = rigthChild;
+    }
+    public Node getRigthChild() {
+        return rigthChild;
+    }
+
+   
+    public int compareTo(int o) {
+        if (this.getData()>o) {
+            return 1;
+        } else if (this.getData()<o) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 
 }
--- src/Trees/ABB.java
+++ src/Trees/ABB.java
@@ -1,5 +1,37 @@
 package Trees;
 
-public class ABB {
+import Node.Node;
 
+public class ABB {
+    public Node root;
+    
+
+    public ABB insert(int data) {
+        // if (contains()) {
+        //     return null;
+        // }
+        root = insert(data, root);
+        return this;
+    }
+
+    public Node insert(int data, Node node) {
+        if (node == null) {
+            return new Node(data);
+        }
+        if (node.compareTo(data) < 0) {
+            //izquierda
+            node.setLeftChild(insert(data, node.getLeftChild()));
+        }else if(node.compareTo(data) > 0){
+            //derecha
+            node.setRigthChild(insert(data, node.getRigthChild()));
+        }   
+
+        return node;
+    }
+
+    // public boolean contains() {
+        
+    // }
+
+   
 }
